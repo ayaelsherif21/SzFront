20d1f8582f223f322817ba012ae40ab61427b3a8
Tefa600 <abdellatifmostafa06@gmail.com>
2023-03-13 12:25:22 +0200

    delete Box,
    added and managed the button delete account

--- src/components/Profile/Guest/userProfile.js
+++ src/components/Profile/Guest/userProfile.js
@@ -1,44 +1,76 @@
 import React from "react";
 import "./Profile.css";
+import deleteAccount from "../../../images/deleteAccount.png"
 
+function PGNavigator() {
+
+}
 
 function UserProfile() {
-    let elements = ['Expand Details', 'Edit ', 'Show History', 'Contact US'];
-    let data = ['Guest Name','GUESTEMAIL@EMAIL.COM', 'GUESTRANK'];
-
-
+    let data = ['Guest Name', 'GUESTEMAIL@EMAIL.COM',];
+    let elements = [{name: 'Expand Details', value: 1},
+        {name: 'Show History', value: 2},
+        {name: 'Contact US', value: 3},
+        {name: 'Log out', value: 4},
+    ];
 
 
     let ProfileDetails = 'This is the details for the user for the meantime';
+
+    function hideDeleteBox() {
+        var element = document.getElementById("deleteBox");
+        element.style.display = "none";
+    }
+
+    function showDeleteBox() {
+        var element = document.getElementById("deleteBox");
+        element.style.display = "flex";
+    }
+
     return (
         // big container
         <div className="bigContainer">
-        {/*    left Container*  for the long detailed guest info*/}
+            <div id={"deleteBox"} className="alerts">
+                <div className="alertBox">
+                    <img src={deleteAccount} alt="Delete Account"/>
+                    <div className="txt mt-3">Do you want to delete your Account?</div>
+                    <div className="alertButtons mt-3">
+                        <button className={'deleteAccount btn btn-danger'}>Delete</button>
+                        <button  className={'btn btn-primary'} onClick={hideDeleteBox}>Cancel</button>
+                    </div>
+                </div>
+
+            </div>
+            {/*    left Container*  for the long detailed guest info*/}
             <div className="leftContainer col-3">
                 <div className="profilePicture mt-1">
                     {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
                     <img src={require('./Profile-Male-PNG.png')} alt="Your Picture" className="guestPic"/>
                 </div>
                 <div className="PGInfo mt-3">
-                    {data.map(function (value, index, array){
+                    {data.map(function (value, index, array) {
                         return <div className="PGData" key={index}>{value}</div>
                     })}
                 </div>
                 <div className={"PGButtonHolder mt-4"}>
-                    {elements.map(function(value, index) {
-                        return <button className={"PGbuttons m-lg-1   btn btn-primary "} key={index}>{value}</button>
+                    {elements.map(function (element, index) {
+                        return <button id={element.value} className={"PGbuttons m-lg-1   btn btn-primary "}
+                                       key={index} onClick={PGNavigator}>{element.name}</button>
                     })}
+                    <button className={"btn btn-danger m-lg-1 PGButtons"} onClick={showDeleteBox} cl>Delete Account</button>
                 </div>
 
+
             </div>
-            <div className="rightContainer col-10">
-                <div className="historyCards mt-5">This is the historty for the guest</div>
+            <div className="rightContainer col-9">
+                <div className="historyCards mt-5">This is the history for the guest</div>
             </div>
-        {/*
+            {/*
         Right Container* for the history and so on
         This box should show every content of the buttons in the left box
-
         */}
+
+
 
         </div>
     );
